3baa21a9416da1d54ee110591d5d7f1f892d5c07
artemisfowl004 <68847756+artemiszuk@users.noreply.github.com>
2022-01-21 15:40:02 +0000

    init

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -4,9 +4,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @app.on_message(filters.command(["start"]) & CustomFilters.auth_users)
 async def start(client, message):
-    text = "**📌MAIN MENU**\n\nHi ! This is Simple File Upload Bot \n\n__Check Below for commands/features__"
     await message.reply(
-        text,
+        Messages.START_MSG,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -27,3 +26,62 @@ async def start(client, message):
 @app.on_message(filters.command(["help"]))
 async def help(client, message):
     await start(client, message)
+
+@app.on_callback_query()
+async def button(client, cmd: CallbackQuery):
+    cb_data = cmd.data
+    if "help" in cb_data:
+        await cmd.message.edit(
+            Messages.HELP_MSG,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Back ◀", callback_data="start")]]
+            ),
+        )
+    elif "start" in cb_data:
+        await cmd.message.edit(
+            Messages.START_MSG,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Help ❓", callback_data="help"),
+                        InlineKeyboardButton(
+                            "Developer 🧑‍💻", url="https://t.me/SorceryBhai"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton("Close ❌", callback_data="close"),
+                        InlineKeyboardButton(
+                            "Source Code 📝",
+                            url="https://github.com/artemiszuk/upload_Z",
+                        ),
+                    ],
+                ]
+            ),
+        )
+    elif "close" in cb_data:
+        await cmd.message.delete()
+    elif "cancel" in cb_data:
+        Var.cancel[cmd.from_user.id] = True
+    elif "toggle" in cb_data:
+        user_id = cmd.from_user.id
+        print(user_id)
+        if Var.upload_as_doc[user_id]:
+            Var.upload_as_doc[user_id] = False
+        else:
+            Var.upload_as_doc[user_id] = True
+        text = "**⚙TOGGLE MENU:**\n[__Click to change__]\n\nVideo File Will be Uploaded as "
+        if Var.upload_as_doc[user_id]:
+            text_append = "Document 📁"
+        else:
+            text_append = "Streamable 🎬"
+        await cmd.message.edit(
+            text,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text_append, callback_data="toggle"),
+                        InlineKeyboardButton("Close ❌", callback_data="close"),
+                    ]
+                ]
+            ),
+        )
